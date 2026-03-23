66920438ca4c65e27c2eb2d70c4cc94709ceb9f7
Gabryso <gabriel.ostaszewski@gmail.com>
2026-03-23 16:13:25 +0100

    Commit z lekcji

---
+++ NetZajW2/Data/PeopleContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetZajW2.Models;
+
+namespace NetZajW2.Data
+{
+    public class HeroesContext : DbContext
+    {
+        public HeroesContext(DbContextOptions options) : base(options) { }
+        public DbSet<Hero> Hero { get; set; }
+    }
+}
